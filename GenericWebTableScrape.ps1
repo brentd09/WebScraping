9704fb8fe418ba93a7bd6ee28a79c4544e493fd3
Brent Denny <brent.denny@ddls.com.au>
2019-05-23 07:25:00 +1000

    Generic html table finding script

--- GenericWebTableScrape.ps1
+++ GenericWebTableScrape.ps1
@@ -2,24 +2,27 @@
 $url = 'https://talosintelligence.com/reputation_center/lookup?search=8.8.8.8'
 $htmlObj = Invoke-WebRequest -UseBasicParsing -Uri $url
 
-[int[]]$TableStarts = @()
-[int[]]$TableEnds = @()
-[int[]]$TableLengths = @()
-
 $singleString = $htmlObj.Content -replace "`n",''
 $indexStart = $singleString.IndexOf("<table")
 $indexEnd   = $singleString.indexof("</table")
 $firstPass = $true
 do {
-  if ($firstPass -eq $true) {$firstPass = $false}
+  if ($firstPass -eq $true) {
+    $firstPass = $false
+    $NextTablestart = $indexStart
+    $NextTableend   = $indexEnd + 8
+    $TableLength = $NextTableend - $NextTablestart
+  }
   else {
     $indexStart = $NextTablestart
     $indexEnd   = $NextTableend
+    $NextTablestart = $singleString.IndexOf("<table",$indexEnd)
+    $NextTableend   = $singleString.IndexOf("</table",($indexEnd+8)) + 8
+    $TableLength = $NextTableend - $NextTablestart
   }
-  $NextTablestart = $singleString.IndexOf("<table",$indexEnd)
-  $NextTableend   = $singleString.IndexOf("</table",($indexEnd+8)) + 8
-  $TableLength = $NextTableend - $NextTablestart
-  $TableStarts += $NextTablestart
-  $TableEnds   += $NextTableend
-  $TableLengths += $TableLength
-} until ($NextTablestart -eq -1)
+  $Hash = [ordered]@{
+  TableStart = $NextTablestart
+  TableEnd   = $NextTableend
+  TableLength = $TableLength
+  }
+  if ($NextTablestart -gt 0) {New-Object -TypeName psobject -Property $Hash}} until ($NextTablestart -eq -1)
