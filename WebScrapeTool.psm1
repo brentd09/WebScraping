15af3d5935997adcf077c9c3059fee742e1b5d83
Brent Denny <brent.denny@ddls.com.au>
2019-05-23 12:44:58 +1000

    added help to webscrape tools

--- WebScrapeTool.psm1
+++ WebScrapeTool.psm1
@@ -1,4 +1,23 @@
-function Find-TablesInHtml {
+function Find-TagsInHtml {
+  <#
+  .SYNOPSIS
+    This command will scrape out HTML content from a Web page
+  .DESCRIPTION
+    The command will use the URL and tag names supplied and strip the content from the
+    website based on the tag name provided, for example if the website has tables in 
+    the page and you want the table data to be displayed; you specify the URL and the 
+    name of the tag which in this case would be TABLE and this would extract each TABLE
+    and setup an object collection showing the entire <table>...</table> so this can 
+    be converted to XML to make the traversal of the tags and data very simple.
+  .EXAMPLE
+    [xml[]]$WebInfo = Find-TagsInHtml -URL 'http://website.com' -HTMLTagName 'table'
+    This will extract all of the TABLES from the URL and will create a XML array.
+  .NOTES
+    General notes
+      Created by: Brent Denny
+      Created on: 23-May-2019
+  #>
+  [cmdletbinding()]
   Param (
     [string]$url = 'https://talosintelligence.com/reputation_center/lookup?search=8.8.8.8',
     [string]$HTMLTagName = 'table'
@@ -26,7 +45,7 @@ function Find-TablesInHtml {
       $NextTableEnd   = $SingleStringNoGaps.IndexOf($HtmlTagClose,($IndexEnd)) + $TagNameCloseLength
       $TableLength = $NextTableEnd - $NextTableStart
     }
-    if ($NextTablestart -gt 0) {
+    if ($NextTablestart -gt 0 -and $NextTableEnd -gt 0 -and $TableLength -gt 0) {
       $Hash = [ordered]@{
         TableStart = $NextTableStart
         TableEnd   = $NextTableEnd
@@ -37,6 +56,3 @@ function Find-TablesInHtml {
     }
   } until ($NextTablestart -eq -1)
 }
-
-$Tables = Find-TablesInHtml
-$Tables 
